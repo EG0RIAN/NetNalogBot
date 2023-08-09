269813bf16dfd4c5e565ad09491bfec9ede38bd6
Egor <e.arkhiptsev@gmail.com>
2023-08-09 23:26:29 +0300

    update

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
+import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from sqlalchemy import create_engine, Column, Integer, String, MetaData
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-import asyncio
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import asyncpg
 
 TOKEN = '6461780172:AAEABfAggnJDYVcFBsHQZJoFb-tNy2axaXY'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 DATABASE_URL = "postgresql://django:django@localhost/admin_bot"
-engine = create_async_engine(DATABASE_URL, echo=True)  # Используем create_async_engine для асинхронной работы
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Создание асинхронного пула соединений с базой данных с помощью asyncpg
+async def open_db():
+    return await asyncpg.create_pool(DATABASE_URL)
+
+async def close_db(pool):
+    await pool.close()
 
 Base = declarative_base()
 
@@ -32,38 +37,42 @@ class Keyword(Base):
     message = Column(String)
     image_path = Column(String)
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all()
 
-async def get_db() -> AsyncSession:
-    async with SessionLocal() as session:
-        async with session.begin():
-            yield session
+# Обработчики событий
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     user = message.from_user
 
-    async with get_db() as db:
-        db_user = User(user_id=user.id, first_name=user.first_name, last_name=user.last_name, username=user.username)
-        db.add(db_user)
-        await db.commit()
+    # Сохранение данных пользователя в базе данных
+    db_user = User(user_id=user.id, first_name=user.first_name, last_name=user.last_name, username=user.username)
+    pool = await open_db()
+    async with pool.acquire() as connection:
+        await connection.execute(
+            "INSERT INTO keywords_users (user_id, first_name, last_name, username) VALUES ($1, $2, $3, $4)",
+            user.id, user.first_name, user.last_name, user.username)
 
-    await message.reply("Привет! Я бот. Отправь мне кодовое слово.")
+    await message.reply("Привет! Я бот. Отправь мне ключевое слово.")
 
 @dp.message_handler(content_types=types.ContentTypes.TEXT)
 async def handle_message(message: types.Message):
     keyword = message.text
 
-    async with get_db() as db:
-        db_keyword = await db.execute(Keyword.__table__.select().where(Keyword.keyword == keyword))
-        db_keyword = await db_keyword.scalar_one_or_none()
+    pool = await open_db()
+    async with pool.acquire() as connection:
+        result = await connection.fetchrow(
+            "SELECT message, image_path FROM keywords_keywords WHERE keyword=$1",
+            keyword)
 
-    if db_keyword:
-        response_message, image_path = db_keyword.message, db_keyword.image_path
+    if result:
+        response_message, image_path = result
         with open(image_path, 'rb') as photo:
             await message.reply_photo(photo, caption=response_message)
     else:
-        await message.reply("Кодовое слово не найдено.")
+        await message.reply("Ключевое слово не найдено.")
+
+    await close_db(pool)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
