432eb839d617052b829371a7f1857dee1004e798
Egor <e.arkhiptsev@gmail.com>
2023-08-09 03:57:52 +0300

    fix

--- bot.py
+++ bot.py
@@ -1,67 +1,71 @@
-import asyncpg
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
 TOKEN = '6461780172:AAEABfAggnJDYVcFBsHQZJoFb-tNy2axaXY'
-DATABASE_URL = 'postgresql://bot:bot@localhost/admin_bot'
-
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
-async def setup_database():
-    return await asyncpg.create_pool(DATABASE_URL)
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-async def insert_user(pool, user):
-    async with pool.acquire() as conn:
-        await conn.execute(
-            "INSERT INTO keywords_users (user_id, first_name, last_name, username) VALUES ($1, $2, $3, $4)",
-            user.id, user.first_name, user.last_name or '', user.username
-        )
+class User(Base):
+    __tablename__ = 'keywords_users'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+    first_name = Column(String)
+    last_name = Column(String)
+    username = Column(String)
 
 
-async def get_response_and_image(pool, keyword):
-    async with pool.acquire() as conn:
-        result = await conn.fetchrow(
-            "SELECT message, image_path FROM keywords_keywords WHERE keywords=$1",
-            keyword
-        )
-        return result
+class Keyword(Base):
+    __tablename__ = 'keywords_keywords'
+
+    id = Column(Integer, primary_key=True)
+    keywords = Column(String, unique=True, nullable=False)
+    message = Column(String)
+    image_path = Column(String)
 
 
-async def on_start(message: types.Message):
+DATABASE_URL = "postgresql://bot:bot@localhost/admin_bot"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
     user = message.from_user
-    pool = message.bot.get('db_pool')
 
-    await insert_user(pool, user)
+    # Save user data to the database
+    session = SessionLocal()
+    new_user = User(user_id=user.id, first_name=user.first_name, last_name=user.last_name, username=user.username)
+    session.add(new_user)
+    session.commit()
+    session.close()
 
     await message.reply("Привет! Я бот. Отправь мне кодовое слово.")
 
 
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
 async def handle_message(message: types.Message):
     keyword = message.text
-    pool = message.bot.get('db_pool')
 
-    result = await get_response_and_image(pool, keyword)
+    session = SessionLocal()
+    result = session.query(Keyword).filter_by(keywords=keyword).first()
+    session.close()
 
     if result:
-        response_message, image_path = result
+        response_message, image_path = result.message, result.image_path
         with open(image_path, 'rb') as photo:
             await message.reply_photo(photo, caption=response_message)
     else:
         await message.reply("Кодовое слово не найдено.")
 
-
-async def on_startup(dp):
-    await bot.send_message(chat_id=739247122, text="Бот запущен")
-
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    pool = loop.run_until_complete(setup_database())
-
-    dp.register_message_handler(start, commands=['start'])
-    dp.register_message_handler(
-        handle_message, content_types=types.ContentTypes.TEXT)
-
     executor.start_polling(dp, skip_updates=True)
