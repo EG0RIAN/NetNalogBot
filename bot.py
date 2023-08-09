973d98bee365332875cff2f0c618334c796b850a
Egor <e.arkhiptsev@gmail.com>
2023-08-09 22:54:55 +0300

    fix

--- bot.py
+++ bot.py
@@ -16,7 +16,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-
 class User(Base):
     __tablename__ = "keywords_users"
 
@@ -26,7 +25,6 @@ class User(Base):
     last_name = Column(String)
     username = Column(String)
 
-
 class Keyword(Base):
     __tablename__ = "keywords_keywords"
 
@@ -34,47 +32,40 @@ class Keyword(Base):
     message = Column(String)
     image_path = Column(String)
 
-
 Base.metadata.create_all(bind=engine)
 
-
-async def get_db():
+def get_db():
     db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
+    return db
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     user = message.from_user
 
     # Сохранение данных пользователя в базе данных
-    db_user = User(
-        user_id=user.id, first_name=user.first_name,
-        last_name=user.last_name,
-        username=user.username)
-    async with get_db() as db:
-        db.add(db_user)
-        db.commit()
+    db_user = User(user_id=user.id, first_name=user.first_name, last_name=user.last_name, username=user.username)
+    db = get_db()
+    db.add(db_user)
+    db.commit()
+    db.close()
 
     await message.reply("Привет! Я бот. Отправь мне кодовое слово.")
-
 
 @dp.message_handler(content_types=types.ContentTypes.TEXT)
 async def handle_message(message: types.Message):
     keyword = message.text
 
-    async with get_db() as db:
-        db_keyword = db.query(Keyword).filter(Keyword.keyword == keyword).first()
+    db = get_db()
+    db_keyword = db.query(Keyword).filter(Keyword.keyword == keyword).first()
 
-        if db_keyword:
-            response_message, image_path = db_keyword.message, db_keyword.image_path
-            with open(image_path, 'rb') as photo:
-                await message.reply_photo(photo, caption=response_message)
-        else:
-            await message.reply("Кодовое слово не найдено.")
+    if db_keyword:
+        response_message, image_path = db_keyword.message, db_keyword.image_path
+        with open(image_path, 'rb') as photo:
+            await message.reply_photo(photo, caption=response_message)
+    else:
+        await message.reply("Кодовое слово не найдено.")
+
+    db.close()
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
