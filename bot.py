12ad11e0d58bbe6d8bde0a03be218ebdafc518f9
Egor <e.arkhiptsev@gmail.com>
2023-08-12 00:50:14 +0300

    update

--- bot.py
+++ bot.py
@@ -1,59 +1,72 @@
+import logging
 import mysql.connector
 from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
 
-TOKEN = '6461780172:AAEABfAggnJDYVcFBsHQZJoFb-tNy2axaXY'
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
-# Replace with your MySQL database connection details
-db_config = {
+# Your MySQL database credentials
+DB_CONFIG = {
     'user': 'bot',
     'password': ')5+,h,X-J5FL',
     'host': 'localhost',
     'database': 'admin_bot',
 }
 
-conn = mysql.connector.connect(**db_config)
+# Initialize the bot and dispatcher
+bot = Bot(token='6461780172:AAEABfAggnJDYVcFBsHQZJoFb-tNy2axaXY')
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
+
+# Create a connection to the MySQL database
+conn = mysql.connector.connect(**DB_CONFIG)
 cursor = conn.cursor()
 
-
+# Handlers
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    user = message.from_user
-
-    # Save user data to the MySQL database
-    first_name = user.first_name
-    last_name = user.last_name if user.last_name else ""
-    username = user.username
-
-    cursor.execute(
-        "INSERT INTO keywords_users (user_id, first_name, last_name, username) VALUES (%s, %s, %s, %s)",
-        (user.id, first_name, last_name, username))
-    conn.commit()
-
-    await message.reply("Привет! Я бот. Отправь мне кодовое слово.")
-
-
-@dp.message_handler(content_types=types.ContentTypes.TEXT)
-async def handle_message(message: types.Message):
-    keyword = message.text
-
-    # Search for the message and image path using the keyword in the MySQL database
-    cursor.execute(
-        "SELECT message, image_path FROM keywords_keywords WHERE keywords=%s",
-        (keyword,))
+    user_id = message.from_user.id
+    first_name = message.from_user.first_name or ' '
+    last_name = message.from_user.last_name or ' '
+    username = message.from_user.username
+    
+    # Check if the user is already in the database
+    cursor.execute("SELECT user_id FROM keywords_users WHERE user_id = %s", (user_id,))
     result = cursor.fetchone()
+    
+    if not result:
+        # Insert the user into the database
+        cursor.execute("INSERT INTO keywords_users (user_id, first_name, last_name, username) VALUES (%s, %s, %s, %s)",
+                       (user_id, first_name, last_name, username))
+        conn.commit()
+    
+    await message.reply("Привет! Это бот!")
 
+@dp.message_handler(lambda message: not message.text.startswith('/'))
+async def handle_keyword(message: types.Message):
+    keyword = message.text
+    
+    # Find the keyword in the database
+    cursor.execute("SELECT * FROM keywords_keywords WHERE keyword = %s", (keyword,))
+    result = cursor.fetchone()
+    
     if result:
-        response_message, image_path = result
-        with open(image_path, 'rb') as photo:
-            await message.reply_photo(photo, caption=response_message)
+        keyword_id, _, response_message, image_path = result
+        image_caption = response_message
+        
+        if image_path:
+            # Send a message with a photo
+            with open(image_path, 'rb') as photo:
+                await message.reply_photo(photo, caption=image_caption)
+        else:
+            # Send a message without a photo
+            await message.reply(image_caption)
     else:
-        await message.reply("Кодовое слово не найдено.")
+        await message.reply("Ключевое слово не найдено.")
 
-
+# Start the bot
 if __name__ == '__main__':
     from aiogram import executor
     executor.start_polling(dp, skip_updates=True)
