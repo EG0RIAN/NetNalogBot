31391af6706eb1c420c8221136ee179e98548452
Egor <e.arkhiptsev@gmail.com>
2023-08-12 00:39:54 +0300

    update

--- bot.py
+++ bot.py
@@ -1,43 +1,58 @@
-import logging
 import mysql.connector
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
 
-# Your Telegram Bot API token
-API_TOKEN = '6461780172:AAEABfAggnJDYVcFBsHQZJoFb-tNy2axaXY'
+TOKEN = '6461780172:AAEABfAggnJDYVcFBsHQZJoFb-tNy2axaXY'
 
-# Your MySQL database configuration
-DB_CONFIG = {
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+# Replace with your MySQL database connection details
+db_config = {
     'user': 'bot',
     'password': ')5+,h,X-J5FL',
     'host': 'localhost',
     'database': 'admin_bot',
 }
 
-logging.basicConfig(level=logging.INFO)
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-db_connection = mysql.connector.connect(**DB_CONFIG)
-db_cursor = db_connection.cursor()
 
-# Handler for any text message
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    user = message.from_user
+
+    # Save user data to the MySQL database
+    first_name = user.first_name
+    last_name = user.last_name if user.last_name else ""
+    username = user.username
+
+    cursor.execute(
+        "INSERT INTO keywords_users (user_id, first_name, last_name, username) VALUES (%s, %s, %s, %s)",
+        (user.id, first_name, last_name, username))
+    conn.commit()
+
+    await message.reply("Привет! Я бот. Отправь мне кодовое слово.")
+
+
 @dp.message_handler(content_types=types.ContentTypes.TEXT)
-async def handle_text(message: types.Message):
-    keyword = message.text.strip()
+async def handle_message(message: types.Message):
+    keyword = message.text
 
-    # Check if the keyword exists in the database
-    db_cursor.execute("SELECT * FROM keywords_keywords WHERE keyword = %s", (keyword,))
-    keyword_data = db_cursor.fetchone()
+    # Search for the message and image path using the keyword in the MySQL database
+    cursor.execute(
+        "SELECT message, image_path FROM keywords_keywords WHERE keywords=%s",
+        (keyword,))
+    result = cursor.fetchone()
 
-    if keyword_data:
-        _, _, response_message, image_path = keyword_data
+    if result:
+        response_message, image_path = result
         with open(image_path, 'rb') as photo:
-            caption = response_message if response_message else None
-            await message.answer_photo(photo, caption=caption)
+            await message.reply_photo(photo, caption=response_message)
     else:
-        await message.answer("Keyword not found.")
+        await message.reply("Кодовое слово не найдено.")
+
 
 if __name__ == '__main__':
     from aiogram import executor
